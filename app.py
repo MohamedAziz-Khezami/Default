bea679abc61bf74f5440722c7c08d9f97d1cefc8
Mohamed Aziz Khezami <khezamimohamedaziz@tbs.u-tunis.tn>
2024-07-05 14:49:13 +0100

    changes

--- app.py
+++ app.py
@@ -158,24 +158,93 @@ def main():
     with st.container():
 
         col1 , col2= st.columns([2,2])
-    
+   
         with col1:
-            a = float(st.text_input("loan amount"))
-            b = float(st.text_input("Rate of interest"))
-            c = float(st.text_input("Interest rate spread"))
-            d = float(st.text_input("Upfront chargs"))
-            e = float(st.text_input("Term"))
-            f = float(st.text_input("Property Value"))
-            g = float(st.text_input("Income"))
-            h = float(st.text_input("Credit Score"))
-            i = float(st.text_input("LTV"))
-            j = float(st.text_input("dtir1"))
+            
+            try:
+                a = float(st.text_input("Loan Amount ($):", value=236500))
+                if a <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                a = None  # Set a to None if invalid input
+
+            try:
+                b = float(st.text_input("Rate of interest:", value=3.6666666666666665))
+                if b <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                b = None  # Set a to None if invalid input
+
+            try:
+                c = float(st.text_input("Interest rate spread", value=0.5148666666666667))
+                if c <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                c = None  # Set a to None if invalid input
+
+            try:
+                d = float(st.text_input("Upfront chargs", value=86.970))
+                if d <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                d = None  # Set a to None if invalid input
+            try:
+                e = float(st.text_input("Term", value=360))
+                if e <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                e = None  # Set a to None if invalid input
+
+            try:
+                f = float(st.text_input("Property Value", value=238000))
+                if f <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                f = None  # Set a to None if invalid input
+
+            try:
+                g = float(st.text_input("Income", value=8640))
+                if g <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                g = None  # Set a to None if invalid input
+            try:
+                h = float(st.text_input("Credit Score", value=848))
+                if h <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                h = None  # Set a to None if invalid input
+            try:
+                i = float(st.text_input("LTV", value=99.3697479))
+                if i <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                i = None  # Set a to None if invalid input
+            try:
+                j = float(st.text_input("dtir1", value=41))
+                if j <= 0:
+                    st.error("Check input")
+                    raise ValueError  # Raise exception to prevent further calculations
+            except (ValueError, TypeError):
+                j = None  # Set a to None if invalid input
+
             ja = st.selectbox('loan limit', ('cf','ncf'))
             k = st.selectbox('Gender', ('Male','Female','Joint','Sex Not Available'))
             
             l = st.selectbox('Approved in Advance', ('nopre','pre'))
             
             m= st.selectbox('Loan type', ('type1','type2','type3'))
+        
+            
             butt = st.button('Check')
 
             
@@ -183,6 +252,7 @@ def main():
 
             
         with col2:
+        
             n = st.selectbox('Loan purpose', ('p1','p2','p3','p4'))
             o = st.selectbox('Credit worthiness', ('l1','l2'))
                         
@@ -198,17 +268,26 @@ def main():
             y = st.selectbox('Age', ('45-54','35-44','55-64','65-74','25-34','>74','<25'))
             z = st.selectbox('Submission of application', ('to_inst','not_inst'))
             za = st.selectbox('Region', ('North','south','central','North-East'))
-            
-            
-            input_ = [a,b,c,d,e,f,g,h,i,j,ja,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,za]
-            
 
-        if  butt:
-            pre = model_input(input_)
-            if pre[0] == 1:
-                st.markdown(f'<h1 style="background-color:#870c03;font-size:24px;">{"🛑This client is likely to default!🛑"}</h1>', unsafe_allow_html=True)
-            else:
-                st.markdown(f'<h1 style="background-color:#1c7501;font-size:24px;">{"This client is not likely to default"}</h1>', unsafe_allow_html=True)
+    
+        
+        input_ = [a,b,c,d,e,f,g,h,i,j,ja,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,za]
+        check = 0
+        for i in input_:
+            if i =='' or i==None:
+                i=None
+                check +=1
+                break
+        if check>0:
+            st.write("Check input")
+
+        else:
+            if  butt:
+                pre = model_input(input_)
+                if pre[0] == 1:
+                    st.markdown(f'<h1 style="background-color:#870c03;font-size:24px;">{"🛑This client is likely to default!🛑"}</h1>', unsafe_allow_html=True)
+                else:
+                    st.markdown(f'<h1 style="background-color:#1c7501;font-size:24px;">{"This client is not likely to default"}</h1>', unsafe_allow_html=True)
 
                     
                     
