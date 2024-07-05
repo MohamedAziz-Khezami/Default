3d371ff5291b500a054674efd8512f60d9fb7c3f
Mohamed Aziz Khezami <khezamimohamedaziz@tbs.u-tunis.tn>
2024-07-05 14:55:00 +0100

    deploy

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.impute import KNNImputer 
-from sklearn.preprocessing import OneHotEncoder, scale, LabelEncoder, StandardScaler
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
 import pickle
-from io import StringIO
+
 
 
 
